5cb777c429d537816afa5a039b34b4e2f541f551
Kamik0714 <dewaathallah2003@gmail.com>
2025-11-02 07:39:51 +0700

    fix download btn

--- Portofolio/script.js
+++ Portofolio/script.js
@@ -1,6 +1,6 @@
 document.getElementById('downloadBtn').addEventListener('click', function() {
   const link = document.createElement('a');
-  link.href = '/resume.pdf';
+  link.href = 'Portofolio\CV\CV.pdf';
   link.download = 'Kamiko-CV.pdf';
   document.body.appendChild(link);
   link.click();

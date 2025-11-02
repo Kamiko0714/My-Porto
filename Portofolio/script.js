f0a53838886b5732aa3444f96e4b9429bc37f97a
Kamik0714 <dewaathallah2003@gmail.com>
2025-11-02 08:26:01 +0700

    downnload fix 4.0

--- Portofolio/script.js
+++ Portofolio/script.js
@@ -1,6 +1,15 @@
 document.getElementById('downloadBtn').addEventListener('click', function() {
+  const lang = document.getElementById('lang-switcher').value; 
   const link = document.createElement('a');
-  link.href = '/CV\Resume.pdf';
+
+  if (lang === 'id') {
+    link.href = '/CV/CV.pdf';
+  } else if (lang === 'en') {
+    link.href = '/CV/Resume.pdf';
+  } else {
+    link.href = '/CV/Resume.pdf';
+  }
+
   link.download = 'Kamiko-CV.pdf';
   document.body.appendChild(link);
   link.click();

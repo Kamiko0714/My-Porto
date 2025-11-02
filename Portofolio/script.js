8e145ea66a491baf8c23a77d0261102959d67ca3
Kamik0714 <dewaathallah2003@gmail.com>
2025-11-02 10:12:25 +0700

    download fxi 5.0

--- Portofolio/script.js
+++ Portofolio/script.js
@@ -1,17 +1,16 @@
 document.getElementById('downloadBtn').addEventListener('click', function() {
-  const lang = document.getElementById('lang-switcher').value; 
+  const lang = document.getElementById('lang-switcher').value;
   const link = document.createElement('a');
 
+  link.style.display = 'none';
   if (lang === 'id') {
-    link.href = '/CV/CV.pdf';
-  } else if (lang === 'en') {
-    link.href = '/CV/Resume.pdf';
+    link.href = 'https://kamiko0714.cyou/CV/CV.pdf';
   } else {
-    link.href = '/CV/Resume.pdf';
+    link.href = 'https://kamiko0714.cyou/CV/Resume.pdf';
   }
-
   link.download = 'Kamiko-CV.pdf';
+
   document.body.appendChild(link);
   link.click();
-  document.body.removeChild(link);
+  setTimeout(() => document.body.removeChild(link), 500);
 });
